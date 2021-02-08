49c0b3b5d46cc3f72cf1245030899d9ddf80bc2b
Michael Dobak <mdobak44@icloud.com>
2021-02-08 18:19:09 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -2,7 +2,7 @@ katz_deli = [ ]
 def line(katz_deli)
  current_line = "the line is currently:"
  katz_deli.each_with_index do |person, i|
-    sub_array << "#{i + 1}. #{person}"
+    current_line << "#{i + 1}. #{person}"
   if katz_deli.length == 0 
     puts "The line is currently empty."
   else 
